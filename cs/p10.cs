951e7ee334f017672ecde459f14cfcaa3cac6d4a
rhtmishra61 <rhtmishra61@gmail.com>
2022-10-20 12:30:53 +0530

    p10 code formatted for readbility

--- cs/p10.cs
+++ cs/p10.cs
@@ -1,4 +1,3 @@
-	
 using System;
 
 public class Program
@@ -11,12 +10,12 @@ public class Program
 		ushort years = 2000;
 		uint days = 730480;
 		ulong hours = 17531520;
-			// print the results on the console
-			Console.WriteLine (centuris + " centuris are " + years + " years, or " + days + " days, or " + hours + " hours ");
+		// print the results on the console
+		Console.WriteLine (centuris + " centuris are " + years + " years, or " + days + " days, or " + hours + " hours ");
 			
-			ulong maxIntValue = UInt64.MaxValue;
-			Console.WriteLine(maxIntValue);
-			// real float-Point Types- Example
+		ulong maxIntValue = UInt64.MaxValue;
+		Console.WriteLine(maxIntValue);
+		// real float-Point Types- Example
 		float floatPI = 3.14f;
 		Console.WriteLine(floatPI);
 		double doublePI = 3.14;
@@ -26,11 +25,11 @@ public class Program
 		double infinity = Double.PositiveInfinity;
 		Console.WriteLine(infinity);
 		//Declare some veriables
-float floatPII = 3.141592653589793238f;
-double doublePII = 3.141592653589793238;
-// Print the results on the console
-Console.WriteLine("Float PII is :" + floatPII);
-Console.WriteLine("Double PII is :" + doublePII);	
+		float floatPII = 3.141592653589793238f;
+		double doublePII = 3.141592653589793238;
+		// Print the results on the console
+		Console.WriteLine("Float PII is :" + floatPII);
+		Console.WriteLine("Double PII is :" + doublePII);	
 		float f = 0.1f;
 		Console.WriteLine(f);
 		double d = 0.1f;
@@ -39,8 +38,7 @@ Console.WriteLine("Double PII is :" + doublePII);
 		Console.WriteLine(ff);
 		double dd = ff;
 		Console.WriteLine(dd);
-	decimal decimalPI = 3.141592653589793284m;
-	Console.WriteLine(decimalPI);	
-	}		
+		decimal decimalPI = 3.141592653589793284m;
+		Console.WriteLine(decimalPI);	
+	}	
 }
-
